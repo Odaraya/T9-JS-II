89d42aa15cc28132a9cd6e5636a4a6220d278d96
K.Lapa <odarayamello@gmail.com>
2020-04-22 15:28:11 -0300

    exercicios

--- Aula - 1/exercicios/highlight8/script.js
+++ Aula - 1/exercicios/highlight8/script.js
@@ -7,5 +7,10 @@
   Dica: pesquise sobre o método de string split, e os métodos de array map e join para ajudar na sua solução.
     
 */
+const paragrafo = document.querySelector('p')
+//console.log(paragrafo.innerText)
+const arrayPalavras = paragrafo.innerText.split("")  // o slipt quebra uma string em um array
+console.log(arrayPalavras)
+
 
 
--- Aula - 1/exercicios/ligaDesliga/script.js
+++ Aula - 1/exercicios/ligaDesliga/script.js
@@ -10,3 +10,21 @@
 
     
 */
+const img = document.querySelector('img')
+const back  = document.body // document.querySelector('body')
+const h1 = document.querySelector('h1')
+
+  backgroundColor.style.backgroundColor="black"
+function changeBg(){
+  if (back.style.backgroundColor =="white"){
+    back.style.backgroundColor ="black"
+    h1.innerText = "Desligado"
+    console.log("Desligado")
+  }else{
+    back.style.backgroundColor ="white"
+    h1.innerText= "ligado"
+    console.log("ligado")
+  }
+  
+}
+
